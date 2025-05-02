baf68ff972cae7b557084b92026b694de0103b6f
Sophia Chang <schang7jcu@gmail.com>
2025-05-01 23:35:03 -0700

    need to now implement monte carlo tree search and winner deciding

--- pokerBot.py
+++ pokerBot.py
@@ -7,25 +7,22 @@ import time
 # gets list of cards ls_cards and community_cards if any
 # returns true or false
 def decideFoldOrStay(ls_cards, community_cards):
-    print("decides whether player will fold or stay")
-
-    # if player == 0, which is the bot, compute Monte Carlo
-    # and return value based on that
-    # based on questions response above
-
+    # compute Monte Carlo
     # returns True if Stay, False for Fold
-    # if the bot has win probability of 50% or over, then stay
-    # else fold
+    # if the bot has win probability of 50% or over, then stay else fold
+    return True if MonteCarloTreeSearch(ls_cards) >= 50 else False
 
 # gets list of cards ls_cards
 def MonteCarloTreeSearch(ls_cards):
     print("apply the mcts")
 
     # continue only for 10 seconds maximum
-    start_time = time.time()
+    """start_time = time.time()
     while time.time() - start_time < 10:
         print("OKKK")
-    print('10 seconds have passed!')
+    print('10 seconds have passed!')"""
+
+    return 50
 
 # gets an integer num_cards_to_lay and set of unusedCards
 # returns the cards in list format
@@ -51,40 +48,32 @@ def poker():
     botCards, unusedCards = layCards(2, unusedCards)
     opponentCards, unusedCards = layCards(2, unusedCards)
     # decide whether players fold or stay
-    """
-        if (not decideFoldOrStay(botCards, None) or not decideFoldOrStay(opponentCards, None)):
-            print("End of gameplay")
-            return 0
-    """
+    if not decideFoldOrStay(botCards, None):
+        print("End of gameplay")
+        return 0
 
     # second round: flop
     community_cards, unusedCards = layCards(3, unusedCards)
-    """
-        if (not decideFoldOrStay(botCards, community_cards) or not decideFoldOrStay(opponentCards, community_cards)):
-            print("End of gameplay")
-            return 0
-    """
+    if not decideFoldOrStay(botCards, community_cards):
+        print("End of gameplay")
+        return 0
 
 
     # third round: turn
     turn_card, unusedCards = layCards(1, unusedCards)
     # add the turn card into the list of community cards
     community_cards += turn_card
-    """
-        if (not decideFoldOrStay(botCards, community_cards) or not decideFoldOrStay(opponentCards, community_cards)):
-            print("End of gameplay")
-            return 0
-    """
+    if not decideFoldOrStay(botCards, community_cards):
+        print("End of gameplay")
+        return 0
 
     # fourth and last round: river
     river_card, unusedCards = layCards(1, unusedCards)
     # add the turn card into the list of community cards
     community_cards += river_card
-    """
-        if (not decideFoldOrStay(botCards, community_cards) or not decideFoldOrStay(opponentCards, community_cards)):
-            print("End of gameplay")
-            return 0
-    """
+    if not decideFoldOrStay(botCards, community_cards):
+        print("End of gameplay")
+        return 0
 
     # decide the winner (since no one has folded at this point!)
     """
